b2976a863ad84195fea1578ffaca532a229b7af3
LiteHell <litehell@litehell.info>
2024-01-30 18:30:33 +0900

    design: change coin count font

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Copyright
  - Noto Sans KR: Copyright (C) Google, licensed under SIL Open Font License
+ - [Press Start](https://www.fontspace.com/press-start-2p-font-f11591): Copyright (c) 2011, Cody "CodeMan38" Boisclair (cody@zone38.net), licensed under SIL Open Font License (OFL)
--- game/src/game/common.rs
+++ game/src/game/common.rs
@@ -29,9 +29,8 @@ pub(crate) fn render_common(context: &mut GameCommonContext) {
 
     // Load a font
     let mut font = ttf_context
-        .load_font("assets/sans.ttf", 32)
+        .load_font("assets/coin.ttf", 24)
         .expect("Unable to load font");
-    font.set_style(sdl2::ttf::FontStyle::BOLD);
 
     // render a surface, and convert it to a texture bound to the canvas
     let text = if context.price == 1 {
@@ -45,19 +44,45 @@ pub(crate) fn render_common(context: &mut GameCommonContext) {
         )
     };
 
+    font.set_outline_width(0);
     let surface = font
         .render(text.as_str())
-        .blended(Color::RGBA(255, 0, 0, 255))
+        .blended(Color::WHITE)
         .expect("Font rendering failure");
+
+    font.set_outline_width(2);
+    let mut outline_surface = font
+        .render(text.as_str())
+        .blended(Color::GRAY)
+        .expect("Font rendering failure");
+
+    let surface_width = surface.width();
+    let surface_height = surface.height();
+    let outline_surface_width = outline_surface.width();
+    let outline_surface_height = outline_surface.height();
+
+    surface
+        .blit(
+            None,
+            &mut outline_surface,
+            Rect::new(
+                ((outline_surface_width - surface_width) / 2) as i32,
+                ((outline_surface_height - surface_height) / 2) as i32,
+                surface_width,
+                surface_height,
+            ),
+        )
+        .unwrap();
+
     let texture = texture_creator
-        .create_texture_from_surface(&surface)
+        .create_texture_from_surface(&outline_surface)
         .expect("Font rendering failure");
 
     canvas.set_draw_color(Color::RGBA(195, 217, 255, 255));
     let TextureQuery { width, height, .. } = texture.query();
     let target = Rect::new(
         ((canvas.viewport().width() - width) / 2) as i32,
-        (canvas.viewport().height() - height - 64) as i32,
+        (canvas.viewport().height() - height - 20) as i32,
         width as u32,
         height as u32,
     );
