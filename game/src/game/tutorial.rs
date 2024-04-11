4fc8dc855e9900e19797ecfe015090e78c09b688
LiteHell <litehell@litehell.info>
2024-04-12 00:16:45 +0900

    stub(tutorial): add stub method for tutorial

---
+++ game/src/game/tutorial.rs
@@ -0,0 +1,5 @@
+use super::{game_common_context::GameCommonContext, game_player::game_result::GameResult};
+
+pub(crate) fn do_tutorial(_common_context: &mut GameCommonContext) {
+    // TO-DO: do tutorial here
+}
