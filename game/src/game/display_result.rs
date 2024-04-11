1ae42934e8f20d257a552e7b37eed3de7adf2bf4
firekann <leehomin118@gmail.com>
2024-04-11 23:53:39 +0900

    feat: add render game result on display_result

--- game/src/game/display_result.rs
+++ game/src/game/display_result.rs
@@ -1,14 +1,15 @@
 use std::path::Path;
 
 use sdl2::{
-    event::Event, image::LoadTexture, keyboard::Keycode, pixels::Color, rect::Rect, render::Canvas,
+    event::Event, image::LoadTexture, keyboard::Keycode, pixels::Color, rect::Rect, render::{Canvas, TextureQuery},
     video::Window,
 };
 
 use super::{
-    common::{render_common},
+    common::render_common,
     game_common_context::GameCommonContext, 
     game_player::game_result::GameResult,
+    util::create_outlined_font_texture::create_outlined_font_texture,
 };
 
 use bidrum_data_struct_lib::song::GameSong;
@@ -36,8 +37,47 @@ fn render_cover_image(canvas: &mut Canvas<Window>, cover_image_path: &Path){
         .expect("Cover image rendering failure");
 }
 
-fn render_score(result: &GameResult) {
-    // TO-DO: render score
+fn render_game_result(canvas: &mut Canvas<Window>, result: &GameResult) {
+    let ttf_context = sdl2::ttf::init().expect("ttf context initialization failure");
+
+    let texture_creator = canvas.texture_creator();
+
+    let mut font = ttf_context
+        .load_font("assets/coin.ttf", 40)
+        .expect("Font loading failure");
+
+    let texts = format!(
+        "{:<9} {:>4}\n{:<9} {:>4}\n{:<9} {:>4}\n{:<9} {:>4}\n{:<9} {:>4}\n{:<9} {:>4}\n{:<9} {:>4}",
+        "Overchaos", result.overchaos_count,
+        "Perfect", result.perfect_count,
+        "Great", result.great_count,
+        "Good", result.good_count,
+        "Bad", result.bad_count,
+        "Miss", result.miss_count,
+        "Combo", result.combo,
+    );
+
+    for (idx, text) in texts.split("\n").enumerate() {
+        let texture = create_outlined_font_texture(
+            &texture_creator,
+            &mut font,
+            text,
+            2,
+            Color::WHITE,
+            Color::GRAY,
+        )
+        .unwrap();
+
+        let TextureQuery { width, height, .. } = texture.query();
+        let canvas_size = canvas.viewport();
+        let target = Rect::new(
+            ((canvas_size.width() as f32/ 10.0) * 5.0) as i32,
+            ((canvas_size.height() as f32/ 16.0)* (4.0 + idx as f32)) as i32,
+            width as u32,
+            height as u32,
+        );
+        canvas.copy(&texture, None, Some(target)).expect("Failure");
+    }
 }
 
 pub(crate) fn display_result(
@@ -45,7 +85,6 @@ pub(crate) fn display_result(
     result: GameResult,
     song_data: &GameSong
 ) {
-    // TO-DO: display result screen
     loop {
         for event in common_context.event_pump.poll_iter() {
             match event {
@@ -63,7 +102,7 @@ pub(crate) fn display_result(
         canvas.clear();
 
         render_cover_image(canvas, Path::new(&song_data.cover_image_filename));
-        render_score(&result);
+        render_game_result(canvas, &result);
         render_common(common_context);
         common_context.canvas.present();
     }
