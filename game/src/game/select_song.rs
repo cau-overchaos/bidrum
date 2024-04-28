5f99212b53557762d80c8d615d2113af2680a822
boulce <boulce.email@gmail.com>
2024-04-28 19:50:03 +0900

    feat: impl select song background rednering
    impl

--- game/src/game/select_song.rs
+++ game/src/game/select_song.rs
@@ -1,4 +1,7 @@
+use std::path::Path;
+
 use bidrum_data_struct_lib::song::GameSong;
+use sdl2::{event::Event, image::LoadTexture, keyboard::Keycode, rect::Rect};
 
 use super::game_common_context::GameCommonContext;
 
@@ -9,10 +12,36 @@ pub(crate) struct SongSelectionResult {
 }
 
 pub(crate) fn select_song(
-    _common_context: &mut GameCommonContext,
+    common_context: &mut GameCommonContext,
     songs: &Vec<GameSong>,
 ) -> SongSelectionResult {
     // TO-DO: render screen, process user input, eventaully return selected song and options
+    let select_song_background_img_path = Path::new("assets/img/select_song_ui/select_song_background.png");
+    let texture_creator =  common_context.canvas.texture_creator();
+    let select_song_background_img_texture = texture_creator
+        .load_texture(select_song_background_img_path)
+        .expect("Background img file not found");
+
+    common_context.canvas.clear();
+    common_context.canvas.copy(&select_song_background_img_texture, None, None)
+    .expect("Failed to render background image");
+
+    common_context.canvas.present();
+
+    
+    'running: loop {
+        for event in common_context.event_pump.poll_iter() {
+                match event {
+                    sdl2::event::Event::KeyDown { 
+                        keycode: Some(Keycode::Escape), 
+                        ..
+                    }  => {
+                        break 'running;
+                    }
+                    _ => {}
+            }
+         }
+    }
 
     return SongSelectionResult {
         selected_level: songs[0].get_chart_levels().unwrap()[0],
