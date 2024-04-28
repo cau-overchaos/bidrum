0872ba2f3f9ebf4dc79c35c843ccd0734f49bb6f
boulce <boulce.email@gmail.com>
2024-04-28 23:22:10 +0900

    feat: impl song_dipsplay_stand redering and common_rendering
    impl

--- game/src/game/select_song.rs
+++ game/src/game/select_song.rs
@@ -1,9 +1,9 @@
 use std::path::Path;
 
 use bidrum_data_struct_lib::song::GameSong;
-use sdl2::{event::Event, image::LoadTexture, keyboard::Keycode, rect::Rect};
+use sdl2::{ image::LoadTexture, keyboard::Keycode, pixels::Color, rect::Rect};
 
-use super::game_common_context::GameCommonContext;
+use super::{common::{event_loop_common, render_common}, game_common_context::GameCommonContext};
 
 pub(crate) struct SongSelectionResult {
     pub selected_song: GameSong,
@@ -16,31 +16,44 @@ pub(crate) fn select_song(
     songs: &Vec<GameSong>,
 ) -> SongSelectionResult {
     // TO-DO: render screen, process user input, eventaully return selected song and options
+    
+    // draw background of select song menu
+    // let canvas  = &mut common_context.canvas;
     let select_song_background_img_path = Path::new("assets/img/select_song_ui/select_song_background.png");
     let texture_creator =  common_context.canvas.texture_creator();
     let select_song_background_img_texture = texture_creator
         .load_texture(select_song_background_img_path)
         .expect("Background img file not found");
-
-    common_context.canvas.clear();
-    common_context.canvas.copy(&select_song_background_img_texture, None, None)
-    .expect("Failed to render background image");
-
-    common_context.canvas.present();
-
+    let viewport = common_context.canvas.viewport();
     
+    // enable alpha blending
+    common_context.canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
+
+    // TODO songs selection menu background line drawing
+    let song_display_stand_width = viewport.width();
+    let song_display_stand_height = viewport.height() / 2;
+    let song_display_stand_x = 0;
+    let song_display_stand_y = viewport.height() / 3;
+    let song_display_stand_background_alpha :u8 = 100;
+    
+
+    // waiting user input
     'running: loop {
         for event in common_context.event_pump.poll_iter() {
-                match event {
-                    sdl2::event::Event::KeyDown { 
-                        keycode: Some(Keycode::Escape), 
-                        ..
-                    }  => {
-                        break 'running;
-                    }
-                    _ => {}
+            if event_loop_common(&event, &mut common_context.coins) {
+                break 'running;
             }
-         }
+        }
+        
+        common_context.canvas.clear();
+        common_context.canvas.copy(&select_song_background_img_texture, None, None)
+        .expect("Failed to render background image");
+
+        common_context.canvas.set_draw_color(Color::RGBA(200, 200, 200, song_display_stand_background_alpha));
+        common_context.canvas.fill_rect(Rect::new(song_display_stand_x, song_display_stand_y as i32, song_display_stand_width, song_display_stand_height))
+        .unwrap();
+        render_common(common_context);
+        common_context.canvas.present();
     }
 
     return SongSelectionResult {
