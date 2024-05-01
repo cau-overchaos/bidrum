57d8f5d5aa0bb1700c3a47cfbd7cb52c81093115
boulce <boulce.email@gmail.com>
2024-05-01 13:01:08 +0900

    feat: impl redering when items are stop
    impl rederingwhen items are stop

--- game/src/game/select_song.rs
+++ game/src/game/select_song.rs
@@ -67,23 +67,26 @@ pub(crate) fn select_song(
     let song_display_stand_x = 0;
     let song_display_stand_y = (viewport.height() / 3) as i32;
     let song_display_stand_background_alpha :u8 = 100;
-    
+
+    // selected song item variables
+    let displayed_selected_song_cnt = 5; // it should be odd number for existence of center song item
+    let song_selection_item_rect_width = song_display_stand_height / 2 ;
+    let song_selection_item_rect_height = song_selection_item_rect_width;
+    let mut selected_item_x = viewport.width() as i32 / 2 - song_selection_item_rect_width as i32 / 2; // '- song_selection_item_rect_width / 2' is for positioing at center
+    let song_selection_item_upper_y = (song_display_stand_y + (song_display_stand_y + song_display_stand_height as i32)) / 3;
+    let mut selecetd_song_item_idx : i32 = 0; // the center item of displayed song item is selected song item
+
+    // cover image variables
+    let cover_img_width = song_selection_item_rect_width * 2 / 3;
+    let cover_img_height = cover_img_width;
+
     // variables for song selection menu moving
     let mut last_key_press_time = Instant::now(); // most recent time when the user press left or right key
     // let mut moving_start_x_pos = 0; // start moving position of song selection item when user press left or right key
     let moving_speed = 1; // moving speed of song selection item
-    let item_interval = 100; // interval between song selection item
-    let moving_distance = item_interval; // maximum moving distance of song selection item 
+    let song_selection_item_interval = (song_display_stand_width / displayed_selected_song_cnt) as i32; // interval between song selection item, adjusting the interval to display displayed_selected_song_cnt number of items on display
+    let moving_distance = song_selection_item_interval; // maximum moving distance of song selection item 
     let mut moving_direction : MovingDirection= MovingDirection::Stop; // moving direction of song selection item
-
-    // let mut song_selection_item_rect = Rect::new(0, 0, 300, 300); // rectangle object of song selection item
-
-    let song_selection_item_rect_width = song_display_stand_height / 2 ;
-    let song_selection_item_rect_height = song_selection_item_rect_width;
-    let mut selected_item_center_x = viewport.width() / 2;
-    let first_song_selection_item_upper_y = (song_display_stand_y + (song_display_stand_y + song_display_stand_height as i32)) / 3;
-    let mut selected_song_idx : i32 = 0; // the center item of displayed song item is selected song item
-    
 
     // song_selection_item_rect.set_y((song_display_stand_y + (song_display_stand_y + song_display_stand_height as i32)) / 3);
     // set_center_x_of_rect(&mut song_selection_item_rect, (viewport.width()/2) as i32);
@@ -132,11 +135,11 @@ pub(crate) fn select_song(
     //             first_song_selection_item_x = (moving_start_x_pos as f32 - current_moved_distance) as u32;
             } else { // after the song selection item moved, the seleceted song is changed
                 moving_direction = MovingDirection::Stop;
-                selected_song_idx +=1;
-                if selected_song_idx >= song_selection_items.len() as i32 {
-                    selected_song_idx = 0;
+                selecetd_song_item_idx +=1;
+                if selecetd_song_item_idx >= song_selection_items.len() as i32 { // for circular array
+                    selecetd_song_item_idx = 0;
                 }
-                println!("{}", selected_song_idx);
+                println!("{}", selecetd_song_item_idx);
             }
        } else if moving_direction == MovingDirection::Right { // if user press left key, then song menu moves to left for specific distance
             let elapsed_time = last_key_press_time.elapsed().as_millis() as f32;
@@ -145,11 +148,11 @@ pub(crate) fn select_song(
     //             first_song_selection_item_x = (moving_start_x_pos as f32 + current_moved_distance) as u32;
             } else { // after the song selection item moved, the seleceted song is changed
                 moving_direction = MovingDirection::Stop;
-                selected_song_idx -=1;
-                if selected_song_idx < 0 {
-                    selected_song_idx = song_selection_items.len() as i32 - 1;
+                selecetd_song_item_idx -=1;
+                if selecetd_song_item_idx < 0 { // for circular array
+                    selecetd_song_item_idx = song_selection_items.len() as i32 - 1;
                 }
-                println!("{}", selected_song_idx);
+                println!("{}", selecetd_song_item_idx);
             }
        }
 
@@ -159,21 +162,39 @@ pub(crate) fn select_song(
         common_context.canvas.copy(&select_song_background_img_texture, None, None)
         .expect("Failed to render background image");
 
-    //     // drawing song selection standing menu
-    //     common_context.canvas.set_draw_color(Color::RGBA(200, 200, 200, song_display_stand_background_alpha));
-    //     common_context.canvas.fill_rect(Rect::new(song_display_stand_x, song_display_stand_y, song_display_stand_width, song_display_stand_height))
-    //     .unwrap();
+        // drawing song selection standing menu
+        common_context.canvas.set_draw_color(Color::RGBA(200, 200, 200, song_display_stand_background_alpha));
+        common_context.canvas.fill_rect(Rect::new(song_display_stand_x, song_display_stand_y, song_display_stand_width, song_display_stand_height))
+        .unwrap();
 
-    //     // drawing song select item
-    //     common_context.canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+        common_context.canvas.set_draw_color(Color::RGBA(200, 200, 200, 240));
         
+    if moving_direction == MovingDirection::Left {
 
-    //     // TODO DRAWING SEVERAL ITEMS, NOT WORKING
-    //    let mut item_x = first_song_selection_item_x as i32;
-    //     for item in &song_selection_items {
-    //         common_context.canvas.fill_rect(Rect::new(item_x, song_display_stand_y, song_display_stand_width, song_display_stand_height)).unwrap();
-    //         // item_x += item_interval;
-    //     }
+    } else if moving_direction == MovingDirection::Right {
+
+    } else {
+        let mut item_x = selected_item_x as i32;
+        for i in selecetd_song_item_idx -(displayed_selected_song_cnt as i32 / 2) .. selecetd_song_item_idx + (displayed_selected_song_cnt as i32 / 2) + 1 {
+            let item_x = selected_item_x + (i - selecetd_song_item_idx)* song_selection_item_interval;
+            common_context.canvas.fill_rect(Rect::new(item_x , song_selection_item_upper_y, song_selection_item_rect_width, song_selection_item_rect_height)).unwrap();
+            
+            let real_song_selection_idx = {
+                let mut idx : usize = 0;
+                if i < 0 {
+                    idx = (i + song_selection_items.len() as i32) as usize;
+                } else if i >= song_selection_items.len() as i32{
+                    idx = (i - song_selection_items.len() as i32) as usize;
+                }
+                idx
+            };
+
+            let cover_img_x = (item_x + (item_x + song_selection_item_rect_width as i32)) / 2 - cover_img_width as i32/2;
+            let cover_img_y: i32 = (song_selection_item_upper_y + (song_selection_item_upper_y + song_selection_item_rect_height as i32)) / 2 - cover_img_height as i32 / 2;
+            let cover_img_rect = Rect::new(cover_img_x, cover_img_y, cover_img_width, cover_img_height);
+            common_context.canvas.copy(&song_selection_items[real_song_selection_idx].cover_img_texture, None, cover_img_rect);
+        }
+    }
 
         // drawing common
         render_common(common_context);
