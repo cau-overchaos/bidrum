116fbddc08050eb6f8e33d3db6a2c47fc7d1013c
LiteHell <litehell@litehell.info>
2024-07-31 20:51:35 +0900

    feat: add price setting

---
+++ GameSettings.cs
@@ -0,0 +1,15 @@
+namespace bidrumgodot;
+
+public class GameSettings
+{
+    /// <summary>
+    /// Price of game
+    /// </summary>
+    public int Price { get; private set; } = 2;
+
+    public bool Uncommericial { get; private set; } = false;
+    public static GameSettings GetSettings()
+    {
+        return new GameSettings();
+    }
+}
--- main.cs
+++ main.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using bidrumgodot;
 using bidrumgodot.controller;
 
 public partial class main : Node
@@ -13,13 +14,59 @@ public partial class main : Node
         _jangguController = GlobalContext.Instance.JangguController;
     }
 
+    private int price()
+    {
+        return GameSettings.GetSettings().Price;
+    }
+
+    /// <summary>
+    /// Avaialble credits for playing game.
+    ///
+    /// For example, If 7 coins are not used yet and the price is 3 coins, return value will be 2. 
+    /// </summary>
+    /// <returns>Available credits for playing game</returns>
+    private int credits()
+    {
+        return _coins / price();
+    }
+
+    /// <summary>
+    /// Leftover coins which cannot be a credit
+    ///
+    /// For example, If 7 coins are not used yet and the price is 3 coins, return value will be 1.
+    /// </summary>
+    /// <returns>Leftover coins</returns>
+    private int leftoverCoins()
+    {
+        return _coins % price();
+    }
+    
     public override void _Process(double delta)
     {
         if (_billAccepter.GetCoins() > 0)
         {
             _billAccepter.ConsumeCoins(1);
             _coins += 1;
-            ((Label)GetNode("Coin")).Text = $"COIN: {_coins}";
         }
+
+        String coinText;
+        if (GameSettings.GetSettings().Uncommericial)
+        {
+            coinText = "FREE PLAY (UNCOMMERCIAL)";
+        }
+        else if (price() == 0)
+        {
+            coinText = "FREE PLAY";
+        }
+        else if (price() == 1)
+        {
+            coinText = $"CREDIT: {_coins}";
+        }
+        else
+        {
+            coinText = $"CREDIT: {credits()} ({leftoverCoins()}/{price()})";
+        }
+
+        ((Label)GetNode("Coin")).Text = coinText;
     }
 }
