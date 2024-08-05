d6926430ef7fe7464e6f197a53d2746995ff114c
LiteHell <litehell@litehell.info>
2024-08-05 21:53:02 +0900

    temp: temporary demo for ChartLine scene

--- game/Game.cs
+++ game/Game.cs
@@ -1,3 +1,4 @@
+using bidrum.controller;
 using Godot;
 
 public partial class Game : Node
@@ -5,7 +6,21 @@ public partial class Game : Node
     [Signal]
     public delegate void GameEndedEventHandler();
 
+    private double temp = 4.0F;
+
     public override void _Process(double delta)
     {
+        temp -= 4.0 * delta;
+        float distance = 9.0F;
+        ChartLineNote[] notes = new ChartLineNote[]
+        {
+            new ChartLineNote() { distance = (float)temp, stick = JangguStick.Left },
+            new ChartLineNote() { distance = (float)temp + distance, stick = JangguStick.Right },
+            new ChartLineNote() { distance = (float)temp + distance * 2, stick = JangguStick.Left },
+            new ChartLineNote() { distance = (float)temp + distance * 2, stick = JangguStick.Right },
+        };
+
+        GetNode<ChartLine>("ChartLine").SetLeftNotes(notes);
+        GetNode<ChartLine>("ChartLine").SetRightNotes(notes);
     }
 }
