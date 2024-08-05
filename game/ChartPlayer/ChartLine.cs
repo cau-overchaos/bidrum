c47697aacd950501cba2338f8c6be417d568f580
LiteHell <litehell@litehell.info>
2024-08-05 21:52:49 +0900

    feat: add a method for setting notes

--- game/ChartPlayer/ChartLine.cs
+++ game/ChartPlayer/ChartLine.cs
@@ -1,5 +1,15 @@
+using System.Collections.Generic;
 using Godot;
 
 public partial class ChartLine : Control
 {
+    public void SetLeftNotes(IEnumerable<ChartLineNote> notes)
+    {
+        GetNode<NoteBackgroundBar>("HBoxContainer/Left").SetNotes(notes);
+    }
+
+    public void SetRightNotes(IEnumerable<ChartLineNote> notes)
+    {
+        GetNode<NoteBackgroundBar>("HBoxContainer/Right").SetNotes(notes);
+    }
 }
